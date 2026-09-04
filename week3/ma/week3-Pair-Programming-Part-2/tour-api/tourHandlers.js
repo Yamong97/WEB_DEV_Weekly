43146e4c81ef94b6550cd5be2f1361ff2366cd01
Yamong Phoo <yamongphoo012@gmail.com>
2026-09-04 22:35:48 +0300

    fix: add week3 pair programming files

---
+++ week3/ma/week3-Pair-Programming-Part-2/tour-api/tourHandlers.js
@@ -0,0 +1,76 @@
+const Tour = require("./tourLib");
+
+const getAllTours = (req, res) => {
+  const tours = Tour.getAll();
+
+  res.json(tours);
+};
+
+const createTour = (req, res) => {
+  const { name, info, image, price, location } = req.body;
+
+  if (!name || !info || !image || !price || !location) {
+    return res.status(400).json({
+      message: "All tour fields are required",
+    });
+  }
+
+  const tour = Tour.addOne(
+    name,
+    info,
+    image,
+    price,
+    location
+  );
+
+  res.status(201).json(tour);
+};
+
+const getTourById = (req, res) => {
+  const tour = Tour.findById(req.params.tourId);
+
+  if (!tour) {
+    return res.status(404).json({ message: "Tour not found" });
+  }
+
+  res.json(tour);
+};
+
+const updateTour = (req, res) => {
+  const { name, info, image, price, location } = req.body;
+
+  if (!name || !info || !image || !price || !location) {
+    return res.status(400).json({
+      message: "All tour fields are required",
+    });
+  }
+
+  const tour = Tour.update(
+    req.params.tourId,
+    req.body
+  );
+
+  if (!tour) {
+    return res.status(404).json({ message: "Tour not found" });
+  }
+
+  res.json(tour);
+};
+
+const deleteTour = (req, res) => {
+  const deleted = Tour.deleteOne(req.params.tourId);
+
+  if (!deleted) {
+    return res.status(404).json({ message: "Tour not found" });
+  }
+
+  res.status(204).send();
+};
+
+module.exports = {
+  getAllTours,
+  createTour,
+  getTourById,
+  updateTour,
+  deleteTour,
+};
---
+++ week3/ma/week3-Pair-Programming-Part-2/tour-api/tourLib.js
@@ -0,0 +1,61 @@
+let tours = [];
+let nextId = 1;
+
+const addOne = (name, info, image, price, location) => {
+  const tour = {
+    id: nextId,
+    name,
+    info,
+    image,
+    price,
+    location,
+  };
+
+  tours.push(tour);
+  nextId++;
+
+  return tour;
+};
+
+const getAll = () => {
+  return tours;
+};
+
+const findById = (id) => {
+  return tours.find((tour) => tour.id === Number(id)) || null;
+};
+
+const update = (id, data) => {
+  const tour = tours.find(
+    (tour) => tour.id === Number(id)
+  );
+
+  if (!tour) {
+    return null;
+  }
+
+  Object.assign(tour, data);
+
+  return tour;
+};
+
+const deleteOne = (id) => {
+  const index = tours.findIndex(
+    (tour) => tour.id === Number(id)
+  );
+
+  if (index === -1) {
+    return false;
+  }
+
+  tours.splice(index, 1);
+  return true;
+};
+
+module.exports = {
+  addOne,
+  getAll,
+  findById,
+  update,
+  deleteOne,
+};
